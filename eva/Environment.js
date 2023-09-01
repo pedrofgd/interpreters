a32e7911024610340ffd8fa9727ef3cbdd45b8ef
Pedro Dias <mnopedrodias@gmail.com>
2023-08-31 21:25:08 -0300

    Lecture 7: Blocks: expression groups and Nested Scopes

--- eva/Environment.js
+++ eva/Environment.js
@@ -1,6 +1,9 @@
 class Environment {
-    constructor(record = {}) { // default is the empty record
+    // Creates and environment with the given record
+    // or an new empty one as default
+    constructor(record = {}, parent = null) {
         this.record = record;
+        this.parent = parent;
     }
 
     // Creates a varuable with the given name and value
@@ -9,13 +12,31 @@ class Environment {
         return value;
     }
 
+    // Updates an existing variable
+    assign(name, value) {
+        this.resolve(name).record[name] = value;
+        return value;
+    }
+
     // Returns the value of a defined variable, or throws
     // if the variable is not defined
     lookup(name) {
-        if (!this.record.hasOwnProperty(name)) {
+        return this.resolve(name).record[name];
+    }
+
+    // Identifier resolution
+    // Returns specific environment in which a variable is defined,
+    // or throws if a variable is not defined
+    resolve(name) {
+        if (this.record.hasOwnProperty(name)) {
+            return this;
+        }
+
+        if (this.parent == null) {
             throw new ReferenceError(`Variable "${name} is not defined"`);
         }
-        return this.record[name];
+
+        return this.parent.resolve(name);
     }
 }
 
