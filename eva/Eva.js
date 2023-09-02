de6d08b06640a602fb7eb0dc2369da76901b86ff
Pedro Dias <mnopedrodias@gmail.com>
2023-09-02 19:33:42 -0300

    Lecture 12: Lambda functions and Functional programming

--- eva/Eva.js
+++ eva/Eva.js
@@ -72,16 +72,27 @@ class Eva {
 
         // -----------------------------------
         // Function declaration: (def square (x) (* x x))
+        //
+        // Syntatic sugar for: (var square (lambda (x) (* x x)))
         if (exp[0] === 'def') {
             const [_tag, name, params, body] = exp;
 
-            const fn = {
+            // JIT-transpile to a variable declaration
+            const varExp = ['var', name, ['lambda', params, body]];
+
+            return this.eval(varExp, env);
+        }
+
+        // -----------------------------------
+        // Lambda function: (lambda (x) (* x x))
+        if (exp[0] == 'lambda') {
+            const [_tag, params, body] = exp;
+
+            return {
                 params,
                 body,
                 env, // Closure!
             };
-
-            return env.define(name, fn);
         }
 
         // -----------------------------------
